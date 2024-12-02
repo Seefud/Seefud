a92ffb13240595f05f60a197eed98758123f801f
RafliOrtegaJaya <m299b4ky3606@bangkit.academy>
2024-12-02 17:44:26 +0700

    Delete Landing Page Web directory

--- Landing Page Web/scripts.js
+++
@@ -1,6 +0,0 @@
-function toggleNavbar() {
-    const navbar = document.getElementById('navbar');
-    const closeIcon = document.getElementById('closeIcon');
-    navbar.classList.toggle('active');
-    closeIcon.classList.toggle('active');
-  }
